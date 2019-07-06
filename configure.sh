6e148d47bce795d03460f757228644b55b9e70af
Pierre Courtieu <Pierre.Courtieu@cnam.fr>
2019-07-06 13:52:50 +0200

    Removing test file from the install target.

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 FILES=`find . -name "*.v" -exec echo {} \;`
 echo "-R LibHyps LibHyps" > _CoqProject
 echo "" >> _CoqProject
-for i in `find . -name "*.v"| grep -v LibHypsNaming2 | grep -v LibHypsExamples | grep -v LibHypsDemo `; do
+for i in `find . -name "*.v"| grep -v LibHypsNaming2 | grep -v LibHypsExamples | grep -v LibHypsDemo | grep -v LibHypsTest `; do
   echo $i >> _CoqProject
 done
 coq_makefile -f _CoqProject -o Makefile
