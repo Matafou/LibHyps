a1c24d2b3112877a9d4c9df2f22cca1f73127c8b
Pierre Courtieu <Pierre.Courtieu@cnam.fr>
2021-06-06 09:23:03 +0200

    Removed tests and demo files from compilation targets.

--- configure.sh
+++ configure.sh
@@ -3,7 +3,7 @@
 FILES=`find . -name "*.v" -exec echo {} \;`
 echo "-R LibHyps LibHyps" > _CoqProject
 echo "" >> _CoqProject
-for i in `find . -name "*.v"| grep -v LibHypsNaming2 | grep -v LibHypsExamples | grep -v LibHypsDemo | grep -v LibHypsTest `; do
+for i in `find LibHyps -name "*.v"| grep -v LibHypsNaming2 | grep -v LibHypsExamples | grep -v LibHypsDemo | grep -v LibHypsTest | grep -v LibHypsRegression`; do
   echo $i >> _CoqProject
 done
 coq_makefile -f _CoqProject -o Makefile
